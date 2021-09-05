6e7f60bb5c56350533ee202f04fec73859c474f9
Bogdana Miletić <bogdana.miletic00@gmail.com>
2021-09-05 13:03:55 +0200

    Pregled selektovane manifestacije
    Na klijentskoj strani uradjen prikaz informacija za izabranu manifestaciju.

--- WebProjekat/WebContent/js/pojedinacniPrikazManifestacije.js
+++ WebProjekat/WebContent/js/pojedinacniPrikazManifestacije.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
 	$.get("../WebProjekat/rest/manifestacije/" + naziv + "/" + datum,
 		function(data, status){
 			console.log(JSON.stringify(data));
+			
+			//dinamicki podesavamo vrednosti za prikaz konkretrne manifestacije
+			$("#naziv").text(data.naziv);
+			$("#tip").text(data.tip);
+			// za poster mozemo automatski da postavimo atribut sa putanjom..$("#poster").
+			$("#brojMesta").text("Broj mesta: " + data.brojMesta);
+			$("#preostaliBrojKarata").text("Preostali broj karata: ");
+			$("#datum").text("Datum i vreme odrzavanja: " +  data.datumIvremeOdrzavanja.dayOfMonth+"."+data.datumIvremeOdrzavanja.monthValue+"."+data.datumIvremeOdrzavanja.year +".  " +data.datumIvremeOdrzavanja.hour+":"+data.datumIvremeOdrzavanja.minute +"h");
+			$("#cena").text("Cena regular karte: " + data.cenaRegularKarte);
+			$("#status").text("Status manifestaijce: " + data.status);
+			
+			let adresaSplit = (data.lokacija.adresa).split(",");
+			$("#adresa").text(adresaSplit[0] );
+			$("#grad").text(adresaSplit[1] +" "+ adresaSplit[2]);
+			
 		}
 	);
 	
