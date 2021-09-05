a58d45b0ce7bd0a1cdac17dbfa77ff3711905088
Bogdana Miletić <bogdana.miletic00@gmail.com>
2021-09-05 22:57:14 +0200

    Heder na pocetnoj stranici
    Na pocetnoj stranici dodat je heder u okviru koga su postavljeni dugmici za logovanje i registraciju.

--- WebProjekat/WebContent/js/pretragaManifestacija.js
+++ WebProjekat/WebContent/js/pretragaManifestacija.js
@@ -167,7 +167,7 @@ function prikaziRezultatePrtrage(data){
 		//sad redu u kojem se trenutno nalazimo dodajemo novu kolonu sa karticom
 		$("#row" + brojacRedova).append( 
 				"<div class=\"column\"> " +
-						"<a href=\"pojedinacnaManifestacija.html?naziv="+ podatak.naziv + "&datum="+podatak.datumIvremeOdrzavanja.year+"-"+podatak.datumIvremeOdrzavanja.monthValue+"-"+podatak.datumIvremeOdrzavanja.dayOfMonth +"  " +podatak.datumIvremeOdrzavanja.hour+":"+podatak.datumIvremeOdrzavanja.minute +"\">" +
+						"<a  href=\"pojedinacnaManifestacija.html?naziv="+ podatak.naziv + "&datum="+podatak.datumIvremeOdrzavanja.year+"-"+podatak.datumIvremeOdrzavanja.monthValue+"-"+podatak.datumIvremeOdrzavanja.dayOfMonth +"  " +podatak.datumIvremeOdrzavanja.hour+":"+podatak.datumIvremeOdrzavanja.minute +"\">" +
 						" <div class=\"card\">" +
 						"	 <p style=\"font-size:25px;\">"+ podatak.naziv +" </p> " +
 							"<hr>" +
