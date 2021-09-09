e73ea224a3eb02323c9d95d4eb77e6e4f97ac530
Bogdana Miletić <bogdana.miletic00@gmail.com>
2021-09-09 21:22:04 +0200

    Pregled svih karata - administrator
    Administratorima je omogucen pregled svih karata >> svih statusa(otkazane i rezervsiane.

--- WebProjekat/WebContent/js/pregledKarataAdminIprodavac.js
+++ WebProjekat/WebContent/js/pregledKarataAdminIprodavac.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
 					pregledRezervisanihKarataProdavac();
 					
 				}
+				else if(data.uloga == "ADMINISTRATOR"){
+					//ADMINISTRATORIMA JE OMOGUCEN PREGLED SVIH KARATA
+					pregledSvihKarata();
+				}
 			}
 		
 	);
@@ -37,6 +41,34 @@ function pregledRezervisanihKarataProdavac(){
 									" </tr>");
 						
 					}
+				}	
+				
+				console.log("Status je: " + status);
+				console.log("Pronadjeno je: " + JSON.stringify(data));
+				
+				
+			}
+		);
+}
+
+function pregledSvihKarata(){
+	$.get(
+			"../WebProjekat/rest/karte/sveKarte",
+			
+			function(data, status){
+				
+				for( let karte of data){
+							$("#tabelaRezervisanihKarata").append("<tr class=\"redovi\">" +
+									"" + "<td >" + karte.id + "</td>" +
+									"" + "<td >" + karte.manifestacijaZaKojuJeRezervisana + "</td>" +
+									"" + "<td >" + karte.datumIvremeManifestaije + "</td>" +
+									"" + "<td >" + karte.cena + "</td>" +
+									"" + "<td >" + karte.kupacImeIprezime + "</td>" +
+									"" + "<td >" + karte.status + "</td>" +
+									"" + "<td >" + karte.tipKarte + "</td>" +
+									
+									" </tr>");
+						
 				}	
 				
 				console.log("Status je: " + status);
